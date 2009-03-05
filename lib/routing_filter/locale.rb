10bdeca43ac0ffdcd7e95595fb2c1bcb211e9419
Tanzeeb Khalili <tanzeeb@tanzeeb-khalilis-macbook.local>
2009-03-05 10:35:19 -0500

    Optimized url matching for locales

--- lib/routing_filter/locale.rb
+++ lib/routing_filter/locale.rb
@@ -5,21 +5,22 @@ module RoutingFilter
   class Locale < Base
     @@default_locale = :en
     cattr_reader :default_locale
-    
+
+    @@locale_match = %r(^/(#{I18n.available_locales.map{|l|l.to_s}.join('|')})(?=/|$))
+		cattr_reader :locale_match    
+
     class << self
       def default_locale=(locale)
         @@default_locale = locale.to_sym
       end
     end
-
-    locale_match = %r(^/(#{I18n.available_locales.map{|l|l.to_s}.join('|')})(?=/|$))
-
+		
     # remove the locale from the beginning of the path, pass the path
     # to the given block and set it to the resulting params hash
     def around_recognize(path, env, &block)
       locale = nil
 
-      path.sub! locale_match do locale = $1; '' end
+      path.sub! @@locale_match do locale = $1; '' end
 
       returning yield do |params|
         params[:locale] = locale if locale
