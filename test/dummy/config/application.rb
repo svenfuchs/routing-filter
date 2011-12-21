90ac270e8a06e4b6c3a00b8dfa5af4ae24306f68
Morton Jonuschat <yabawock@gmail.com>
2011-12-21 09:06:44 +0100

    Make test application rails 3.0 compatible

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -48,12 +48,6 @@ module TestRailsAdapter
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
 
