4c63f163baf73f6df1618412db1c69b5e233add4
Morton Jonuschat <yabawock@gmail.com>
2011-12-21 01:09:26 +0100

    Test application that gets initialized with rails 3.1/3.2

--- test/test_adapters/rails_3.rb
+++ test/test_adapters/rails_3.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
-require "rails"
+require 'rails/all'
 require 'rack/test'
+
+Bundler.require
 
 module TestRailsAdapter
   include ::Rack::Test::Methods
 
-  APP = Class.new(Rails::Application).tap do |app|
-    app.config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
-    app.config.session_store :cookie_store, :key => "_myapp_session"
-    app.config.active_support.deprecation = :log
-    app.routes.draw do
+  class Application < Rails::Application
+    config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
+    config.session_store :cookie_store, :key => "_myapp_session"
+    config.active_support.deprecation = :log
+    config.i18n.default_locale = :en
+    routes.draw do
       match "/" => "rails_test/tests#index"
       match "/foo/:id" => "rails_test/tests#show", :as => 'foo'
       filter :uuid, :pagination ,:locale, :extension
     end
-    app.initialize!
   end
 
   def app
-    APP
+    ::Rails.application
   end
 
   def response
     last_response
   end
 end
+
+TestRailsAdapter::Application.initialize!
