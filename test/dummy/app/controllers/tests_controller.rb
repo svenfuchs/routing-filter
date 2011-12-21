0f26aee2d045563fd8b28b9d49c50582230fdb19
Morton Jonuschat <yabawock@gmail.com>
2011-12-21 09:07:01 +0100

    The url helpers are already available

--- test/dummy/app/controllers/tests_controller.rb
+++ test/dummy/app/controllers/tests_controller.rb
@@ -1,6 +1,4 @@
 class TestsController < ActionController::Base
-  include Rails.application.routes.url_helpers if defined?(Rails)
-
   def index
     url = url_for(params.merge(:only_path => true))
     render :text => params.merge(:url => url).inspect
