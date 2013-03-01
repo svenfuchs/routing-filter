456f99fc715e1f877944e28acb1656c9a4b19248
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-03-01 18:42:27 +0900

    Skip requiring active_resource in Rails 4

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
+require "active_resource/railtie" if Rails::VERSION::MAJOR < 4
 require "rails/test_unit/railtie"
 
 Bundler.require
