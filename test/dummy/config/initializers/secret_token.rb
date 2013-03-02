2706a7a4a0403889dc4456f0984876cc734343da
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-03-02 17:46:16 +0900

    Avoid warning about `secret_token` in Rails 4
    When `config.secret_token` is used, rails 4 puts the following warning.
    
    ```
    DEPRECATION WARNING: You didn't set config.secret_key_base in
    config/initializers/secret_token.rb file. This should be used instead of
    the old deprecated config.secret_token in order to use the new
    EncryptedCookieStore. To convert safely to the encrypted store (without
    losing existing cookies and sessions)
    ```

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TestRailsAdapter::Application.config.secret_token = '25864837d368f2f52da3bc94063344da5fb52986c9f5e49fa564ee29cd8d9efa2e98f374ee6abcc09e676ab74c07e72fd08dc5af51845aded43c13af6d6497ad'
+method = if TestRailsAdapter::Application.config.respond_to?(:secret_key_base=)
+  :secret_key_base=
+else
+  :secret_token=
+end
+
+TestRailsAdapter::Application.config.send(method, '25864837d368f2f52da3bc94063344da5fb52986c9f5e49fa564ee29cd8d9efa2e98f374ee6abcc09e676ab74c07e72fd08dc5af51845aded43c13af6d6497ad')
