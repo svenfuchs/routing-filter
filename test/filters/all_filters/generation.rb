514bb2ede09cbbb967d2e8bbae1c69d9890f8505
Hamed Asghari <hasghari@gmail.com>
2020-12-24 06:17:39 -0700

    Update tests to use path_for instead of generate

--- test/filters/all_filters/generation.rb
+++ test/filters/all_filters/generation.rb
@@ -1,42 +1,42 @@
 module Generation
   test 'generates the path /some.html (extension)' do
     params = self.params
-    assert_generates '/some.html', routes.generate(params)
+    assert_generates '/some.html', routes.path_for(params)
   end
-  
+
   # extension with any
-  
+
   test 'generates the path /de/some (extension, locale)' do
     params = self.params.merge(:locale => 'de')
-    assert_generates '/de/some.html', routes.generate(params)
+    assert_generates '/de/some.html', routes.path_for(params)
   end
 
   test 'generates the path /some/page/2 (extension, pagination)' do
     params = self.params.merge(:page => 2)
-    assert_generates '/some/page/2.html', routes.generate(params)
+    assert_generates '/some/page/2.html', routes.path_for(params)
   end
 
   test 'generates the path /:uuid/some (extension, uuid)' do
     params = self.params.merge(:uuid => uuid)
-    assert_generates "/#{uuid}/some.html", routes.generate(params)
+    assert_generates "/#{uuid}/some.html", routes.path_for(params)
   end
-  
+
   # extension, locale with any
-  
+
   test 'generates the path /de/some/page/2 (extension, locale, pagination)' do
     params = self.params.merge(:locale => 'de', :page => 2)
-    assert_generates '/de/some/page/2.html', routes.generate(params)
+    assert_generates '/de/some/page/2.html', routes.path_for(params)
   end
-  
+
   test 'generates the path /de/:uuid/some (extension, locale, uuid)' do
     params = self.params.merge(:locale => 'de', :uuid => uuid)
-    assert_generates "/de/#{uuid}/some.html", routes.generate(params)
+    assert_generates "/de/#{uuid}/some.html", routes.path_for(params)
   end
-  
+
   # all
-  
+
   test 'generates the path /de/some/page/2 (extension, pagination, uuid)' do
     params = self.params.merge(:locale => 'de', :page => 2, :uuid => uuid)
-    assert_generates "/de/#{uuid}/some/page/2.html", routes.generate(params)
+    assert_generates "/de/#{uuid}/some/page/2.html", routes.path_for(params)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,19 +9,13 @@ require 'routing_filter'
 require 'minitest/autorun'
 require 'test_declarative'
 
-I18n.enforce_available_locales = false
+require 'test_adapters/rails'
 
-module GenerateFix
-  def generate(options, recall = {})
-    super(options.delete(:use_route), options, recall)
-  end
-end
+I18n.enforce_available_locales = false
 
 class MiniTest::Test
   def draw_routes(&block)
-    instance = ActionDispatch::Routing::RouteSet.new.tap { |set| set.draw(&block) }
-    instance.extend(GenerateFix)
-    instance
+    ActionDispatch::Routing::RouteSet.new.tap { |set| set.draw(&block) }
   end
 
   def assert_generates(expected_path, generated_path)
@@ -38,7 +32,6 @@ class MiniTest::Test
 
     generated_path, extra_keys = generated_path if generated_path.is_a?(Array)
     generated_path << "?#{extra_keys.to_query}" unless extra_keys.blank?
-    message ||= ''
     msg = "The generated path #{generated_path} did not match #{expected_path}"
     assert_equal(expected_path, generated_path, msg)
   end
